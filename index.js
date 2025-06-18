1c668f5c85646cfe9b470ed5fb59d0a7abb9cba6
rakibhossain2025 <rakibhossain4230@gmail.com>
2025-06-18 15:36:38 +0600

    code push kora dan

--- index.js
+++ index.js
@@ -8,24 +8,28 @@ const port = process.env.PORT || 2002;
 app.use(cors());
 app.use(express.json());
 
-// const verifyToken = async (req, res, next) => {
-  
-//   const authHeader = req.headers.authorization
-//   if (!authHeader) {
-//     return res.status(401).send({ message: 'bro message (if unauthorize )' })
-//   }
+const admin = require("firebase-admin");
+const decoded = Buffer.from(process.env.fb_key, 'base64').toString('utf-8')
+const serviceAccount = JSON.parse(decoded)
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-//   try {
-//     const token = authHeader.split(' ')[1]
-//     const decodedUser = await admin.auth().verifyIdToken(token)
-//     req.decoded = decodedUser
-//     next()
+const verifyToken = async (req, res, next) => {
 
-//   } catch (e) {
-//     res.status(401).send({ message: "bro message (if Invalid or expired token)" });
-//   }
-
-// }
+  const authHeader = req.headers.authorization
+  if (!authHeader) {
+    return res.status(401).send({ message: 'bro message (if unauthorize )' })
+  }
+  try {
+    const token = authHeader.split(' ')[1]
+    const decodedUser = await admin.auth().verifyIdToken(token)
+    req.decoded = decodedUser
+    next()
+  } catch (e) {
+    res.status(401).send({ message: "bro message (if Invalid or expired token)" });
+  }
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iy8gzcy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -53,11 +57,15 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/take-assignment", async (req, res) => {
+    app.get("/my-assignment", verifyToken, async (req, res) => {
+      res.send(await takeAssignment.find({ examineEmail: req.query.email }).toArray());
+    });
+
+    app.post("/take-assignment", verifyToken, async (req, res) => {
       res.send(await takeAssignment.insertOne(req.body));
     });
 
-    app.patch('/mark/:id', async (req, res) => {
+    app.patch('/mark/:id', verifyToken, async (req, res) => {
       try {
         const id = req.params.id;
         const updatedInfo = req.body;
@@ -68,9 +76,9 @@ async function run() {
             $set: {
               givenMark: updatedInfo.givenMark,
               feedback: updatedInfo.feedback,
-              examiner: updatedInfo.examiner,
+              examinerName: updatedInfo.examinerName,
               status: updatedInfo.status,
-              examinerEmail:updatedInfo.examinerEmail
+              examinerEmail: updatedInfo.examinerEmail
             }
           }
         );
@@ -82,31 +90,36 @@ async function run() {
       }
     });
 
-    app.get("/my-assignment", async (req, res) => {
-      res.send(await takeAssignment.find({ examineEmail: req.query.email }).toArray());
+    app.delete("/assignments/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const requesterEmail = req.query.email;
+
+      const assignment = await assignmentsCollection.findOne({
+        _id: new ObjectId(id),
+      });
+
+      if (assignment.createdByEmail !== requesterEmail) {
+        return res.status(403).send({
+          error: "Forbidden: You can only delete your own assignments.",
+        });
+      }
+
+      const result = await assignmentsCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
     });
 
-
-    //! brother's code 
-    //
-
+    app.post("/assignments", verifyToken, async (req, res) => {
+      const newAssignment = req.body;
+      const result = await assignmentsCollection.insertOne(newAssignment);
+      res.send(result);
+    });
 
     app.get("/assignments/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await assignmentsCollection.findOne(query);
-      res.send(result);
-    });
-
-
-    app.post("/myattemps", async (req, res) => {
-      res.send(await MyAttemptsCollection.insertOne(req.body));
-    });
-    // 
-
-    app.post("/assignments", async (req, res) => {
-      newAssignment = req.body;
-      const result = await assignmentsCollection.insertOne(newAssignment);
       res.send(result);
     });
 
@@ -124,40 +137,15 @@ async function run() {
         options
       );
       res.send(result);
-    });
+    })
 
-    // ORG
-    // app.delete("/assignments/:id", async(req, res) => {
-    //   const id = req.params.id;
-    //   const query = {_id: new ObjectId(id)}
-    //   const result = await assignmentsCollection.deleteOne(query);
-    //   res.send(result)
-    // })
+    //! brother's code 
 
-    app.delete("/assignments/:id", async (req, res) => {
-      const id = req.params.id;
-      const requesterEmail = req.query.email;
-
-      const assignment = await assignmentsCollection.findOne({
-        _id: new ObjectId(id),
-      });
-
-      if (assignment?.createdBy !== requesterEmail) {
-        return res.status(403).send({
-          error: "Forbidden: You can only delete your own assignments.",
-        });
-      }
-
-      const result = await assignmentsCollection.deleteOne({
-        _id: new ObjectId(id),
-      });
-      res.send(result);
-    });
   } finally { }
 }
 run().catch(console.dir);
 
-app.get("/", (rakib, res) => {
+app.get("/", (_, res) => {
   res.send("rakib's Server is Getting Now cool😎");
 });
 
